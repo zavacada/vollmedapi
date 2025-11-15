c235b3404493fcf124e55713f023ed35ef2602ac
zavacada <jcfcardoso5555@gmail.com>
2025-11-15 17:39:07 -0300

    feat: Produção de dados na API

--- src/main/java/med/voll/api/medico/Medico.java
+++ src/main/java/med/voll/api/medico/Medico.java
@@ -6,7 +6,6 @@ import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import med.voll.api.endereco.Endereco;
-import med.voll.api.medico.Especialidade;
 
 @Table(name = "medicos")
 @Entity(name = "Medico")
